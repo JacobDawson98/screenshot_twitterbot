7df036534dc6df52404cef22c8fa1ea31e778de2
JacobDawson98 <dawson@iastate.edu>
2018-12-15 03:01:11 -0600

    Feature: Can download seasons 1 - 7 screens

--- twitterbot.py
+++ twitterbot.py
@@ -8,6 +8,7 @@ API_KEY = environ.get('API_KEY')
 API_SECRET = environ.get('API_SECRET')
 OAUTH_TOKEN = environ.get('OAUTH_TOKEN')
 OAUTH_TOKEN_SECRET = environ.get('OAUTH_TOKEN_SECRET')
+
 auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
 auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 api = tweepy.API(auth)
@@ -15,5 +16,10 @@ api = tweepy.API(auth)
 image_dir = get_random_image_dir()
 api.update_with_media(image_dir)
 new_image_dir = path.abspath(
-        path.join(path.dirname(__file__), 'public', 'screens', 'used', image_dir.rsplit('/', 1)[-1]))
+        path.join(
+            path.dirname(__file__),
+            'public',
+            'screens',
+            'used',
+            image_dir.rsplit('/', 1)[-1]))
 rename(image_dir, new_image_dir)
