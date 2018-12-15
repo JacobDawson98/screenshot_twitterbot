4b3b77f388c59ac5c6eddaecf335470a9ca227ef
ejnoring <evanjnoring@gmail.com>
2018-12-14 22:42:01 -0600

    Feature: saves screenshot to public/screen

--- screen_picker/screenpicker.py
+++ screen_picker/screenpicker.py
@@ -1,12 +1,22 @@
 from random import randint
 import cv2
+from os import path
 
-vidcap = cv2.VideoCapture('babys_first_script.mp4')
-num_frames = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
-rand_frame = randint(0, num_frames)
-while rand_frame % 24 != 0:
+def frame_cap():
+    #video_directory = '../public/video'
+    base_dir = path.dirname(__file__)
+    videos_dir = path.abspath(path.join(base_dir, '..', 'public', 'videos', 'babys_first_script.mp4'))
+    print(videos_dir)
+    vidcap = cv2.VideoCapture(videos_dir)
+    num_frames = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
     rand_frame = randint(0, num_frames)
-success,image = vidcap.read()
-cv2.imwrite("frame%d.jpg" % rand_frame, image)
-print('Read a new frame: ', success)
-print(rand_frame)
+    while rand_frame % 24 != 0:
+        rand_frame = randint(0, num_frames)
+    success,image = vidcap.read()
+    screens_path = path.abspath(path.join(base_dir, '..', 'public', 'screens'))
+    print('bigfoot')
+    cv2.imwrite(path.join(screens_path, "frame%d.jpg" % rand_frame), image)
+    print('Read a new frame: ', success)
+    print(rand_frame)
+
+frame_cap()
--- screen_picker/tests.py
+++ screen_picker/tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from .screen_picker import ScreenPicker
+import ScreenPicker
 
 
 class TestScreenPicker(unittest.TestCase):
