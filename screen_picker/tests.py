ebaba9061b68e68926e26b8c596ded9bbd06fb69
JacobDawson98 <dawson@iastate.edu>
2018-12-14 20:34:41 -0600

    Test: Init tests

---
+++ screen_picker/tests.py
@@ -0,0 +1,10 @@
+import unittest
+
+from .screen_picker import ScreenPicker
+
+
+class TestScreenPicker(unittest.TestCase):
+
+    def test_using_correct_directory(self):
+        print('big dogs')
+        self.fail()
