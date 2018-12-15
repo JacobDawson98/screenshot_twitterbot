f0b88f983729e92a5ba99107965d96876ae1a8c4
JacobDawson98 <dawson@iastate.edu>
2018-12-14 22:55:25 -0600

    Refactor: Styling and redundancy

--- screen_picker/screenpicker.py
+++ screen_picker/screenpicker.py
@@ -2,21 +2,18 @@ from random import randint
 import cv2
 from os import path
 
+
 def frame_cap():
-    #video_directory = '../public/video'
-    base_dir = path.dirname(__file__)
-    videos_dir = path.abspath(path.join(base_dir, '..', 'public', 'videos', 'babys_first_script.mp4'))
-    print(videos_dir)
-    vidcap = cv2.VideoCapture(videos_dir)
+    public_dir = path.abspath(path.join(path.dirname(__file__), '..', 'public'))
+    vidcap = cv2.VideoCapture(path.abspath(path.join(public_dir, 'videos', 'babys_first_script.mp4')))
     num_frames = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
     rand_frame = randint(0, num_frames)
     while rand_frame % 24 != 0:
         rand_frame = randint(0, num_frames)
-    success,image = vidcap.read()
-    screens_path = path.abspath(path.join(base_dir, '..', 'public', 'screens'))
-    print('bigfoot')
-    cv2.imwrite(path.join(screens_path, "frame%d.jpg" % rand_frame), image)
-    print('Read a new frame: ', success)
-    print(rand_frame)
+    cap_successful = False
+    while not cap_successful:
+        cap_successful, image = vidcap.read()
+    cv2.imwrite(path.abspath(path.join(public_dir, 'screens', 'frame{}.jpg'.format(rand_frame))), image)
+
 
 frame_cap()
