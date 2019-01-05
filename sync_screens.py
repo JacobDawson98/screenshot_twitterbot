66e3b3a470a90e1eefca567954ee1a82d0b8f0ef
JacobDawson98 <dawson@iastate.edu>
2019-01-05 10:10:43 -0600

    Enhancement: Update used_screens.txt when an image is tweeted

--- sync_screens.py
+++ sync_screens.py
@@ -3,18 +3,18 @@ from os import path, listdir, rename
 
 screens_dir = path.abspath(path.join(path.dirname(__file__), 'public', 'screens'))
 used_dir = path.abspath(path.join(screens_dir, 'used'))
-used_screens_file = 'used_screens.txt'
+default_file = 'used_screens.txt'
 
 
-def create_used_screens_list():
-    with open(used_screens_file, 'a') as used_screens:
+def create_used_screens_list(used_screens_file=None):
+    with open(used_screens_file if used_screens_file else default_file, 'a') as used_screens:
         for f in listdir(used_dir):
             if path.isfile(path.join(used_dir, f)):
                 used_screens.write(f + '\n')
 
 
-def update_screens_dir():
-    with open(used_screens_file) as used_screens:
+def update_screens_dir(used_screens_file=None):
+    with open(used_screens_file if used_screens_file else default_file) as used_screens:
         lines = [l.strip() for l in used_screens.readlines()]
         for image_name in lines:
             image_dir = path.join(screens_dir, image_name)
--- twitterbot.py
+++ twitterbot.py
@@ -3,11 +3,18 @@ from dotenv import load_dotenv
 from os import path, environ, rename, listdir
 from random import choice
 
+from sync_screens import create_used_screens_list
+
 
 def get_random_image_dir():
     screens_dir = path.abspath(path.join(path.dirname(__file__), 'public', 'screens'))
     random_file = choice([f for f in listdir(screens_dir) if path.isfile(path.join(screens_dir, f))])
     return path.abspath(path.join(screens_dir, random_file))
+
+
+def _append_used_screen(image_name):
+    with open('used_screens.txt', 'a') as used_screens:
+        used_screens.write(image_name + '\n')
 
 
 load_dotenv(path.abspath(path.join(path.dirname(__file__), '.env')))
@@ -22,11 +29,18 @@ api = tweepy.API(auth)
 
 image_dir = get_random_image_dir()
 api.update_with_media(image_dir)
+image_name = image_dir.rsplit('/', 1)[-1]
 new_image_dir = path.abspath(
         path.join(
             path.dirname(__file__),
             'public',
             'screens',
             'used',
-            image_dir.rsplit('/', 1)[-1]))
+            image_name))
 rename(image_dir, new_image_dir)
+
+try:
+    _append_used_screen(image_name)
+except FileNotFoundError:
+    create_used_screens_list()
+    _append_used_screen(image_name)
